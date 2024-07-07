1c12b069fee77e1aeab394083405c92585b1e98e
Olman Gemechu <122857604+simera7@users.noreply.github.com>
2024-07-08 01:10:39 +0300

    Time: 36 ms (93.93%), Space: 16.5 MB (31.2%) - LeetHub

--- 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,17 +1,7 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
-        ranges.sort(key=lambda x: x[0])
-        left_1 = min(ranges, key=lambda x: x[0])[0]
-        right_1 = max(ranges, key=lambda x: x[1])[1]
-        print(ranges)
-        for i in range(0, len(ranges)):
-            if ranges[i][1] < left:
-                continue
-            if ranges[i][0] > left:
-                return False
-            if ranges[i][1] >= right:
-                return True
-            if ranges[i][0] <= left:
-                left = ranges[i][1] + 1
-        if not ((left_1 <= left) and (right_1 >= right)):
-            return False
+        set_1={x for x in range(left,right+1)}
+        set_2=set()
+        for start,end in ranges:
+            set_2 |= {x for x in range(start,end+1)}
+        return set_1 <= set_2
