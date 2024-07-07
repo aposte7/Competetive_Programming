44affd64b382fde17331a08cbeb6f8a306a8ed3a
Olman Gemechu <122857604+simera7@users.noreply.github.com>
2024-07-08 01:21:13 +0300

    Time: 41 ms (74.35%), Space: 16.6 MB (8.84%) - LeetHub

--- 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,6 +1,7 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
         set_1={x for x in range(left,right+1)}
-        for start, end in ranges:
-            set_1 -= {x for x in range(start, end + 1)}
-        return not set_1
+        set_2=set()
+        for start,end in ranges:
+            set_2 |= {x for x in range(start,end+1)}
+        return set_1 <= set_2
