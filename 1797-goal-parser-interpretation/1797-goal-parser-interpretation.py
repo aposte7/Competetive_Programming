05b400b5d47da3575f90e033d156e4be1423ae3e
Olman Gemechu <122857604+simera7@users.noreply.github.com>
2024-07-08 15:50:15 +0300

    Time: 41 ms (14.24%), Space: 16.6 MB (16.07%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -2,8 +2,6 @@ class Solution:
     def interpret(self, command: str) -> str:
         result = ""
         for i in range(len(command)):
-            if command[i]==")" or command[i]=="a" or command[i]=="l":
-                continue
             if command[i]=="G":
                 result += "G"
             if command[i]=="(":
