8b05a1f85512c4995151c32a5291426424b2871d
Olman Gemechu <122857604+simera7@users.noreply.github.com>
2024-07-23 03:36:37 +0300

    Time: 34 ms (66.15%), Space: 16.5 MB (16.92%) - LeetHub

--- 1449-print-words-vertically/1449-print-words-vertically.py
+++ 1449-print-words-vertically/1449-print-words-vertically.py
@@ -4,7 +4,6 @@ class Solution:
         if len(s) <= 0:
             return
         hold = s.split(" ")
-        max_len = max(len(item) for item in hold)
         result = ["".join(item).rstrip() for item in itertools.zip_longest(*hold,fillvalue=" ")]
         return result
 
