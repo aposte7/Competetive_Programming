0841a8be8c6670eac3583f949e3961ba8b758355
Olman Gemechu <122857604+simera7@users.noreply.github.com>
2024-07-23 03:35:35 +0300

    Time: 36 ms (52%), Space: 16.4 MB (50.15%) - LeetHub

--- 1449-print-words-vertically/1449-print-words-vertically.py
+++ 1449-print-words-vertically/1449-print-words-vertically.py
@@ -1,18 +1,11 @@
+import itertools
 class Solution:
     def printVertically(self, s: str) -> List[str]:
         if len(s) <= 0:
             return
         hold = s.split(" ")
         max_len = max(len(item) for item in hold)
-        for i, item in enumerate(hold):
-            if len(item) < max_len:
-                value = item + (" " * (max_len - len(item)))
-                hold[i] = value
-    
-        result = ["".join(item) for item in zip(*hold)]
-        
-        for i, item in enumerate(result):
-            result[i] = item.rstrip()
+        result = ["".join(item).rstrip() for item in itertools.zip_longest(*hold,fillvalue=" ")]
         return result
 
             
