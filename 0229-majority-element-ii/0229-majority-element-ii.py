720bd69a974099bd1ac823f80bc5576442a2cf4a
Olman Gemechu <122857604+simera7@users.noreply.github.com>
2024-07-26 10:11:22 +0300

    Time: 85 ms (98.43%), Space: 17.9 MB (52.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,7 +2,5 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         x = Counter(nums)
-
-        l=filter(lambda i:x.get(i) > len(nums) / 3, x)
-        return l
+        return [i for i in x.keys() if x.get(i) > len(nums)/3]
         
