790745dca148c46a9fb02b674c4a07760e70597e
Olman Gemechu <122857604+aposte7@users.noreply.github.com>
2024-10-08 18:53:56 +0300

    Time: 61 ms (9.24%), Space: 16.9 MB (8.81%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,13 +1,20 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
         length  = len(nums)
+        z =0
         for i in range(len(nums)-1):
             if(nums[i]==nums[i+1]):
                 nums[i] =  nums[i]*2
                 nums[i+1] = 0
-          
-        y = [x for x in nums if x != 0]
-        y = y + [0] * (length-len(y))
-        return y
+            if(nums[i]!=0 and nums[z]==0):
+                nums[z], nums[i] = nums[i], nums[z]
+                z+=1
+            if nums[z]!=0:
+                z+=1
+        if(nums[len(nums)-1]!=0 and nums[z]==0):
+                nums[z], nums[len(nums)-1] = nums[len(nums)-1], nums[z]
+                z+=1
+        
+        return nums
         
         
