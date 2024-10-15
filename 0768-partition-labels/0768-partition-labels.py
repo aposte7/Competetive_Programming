0e78cb1c16ee9d63e6bd8662f2c7d36a4d57c60e
Olman Gemechu <122857604+aposte7@users.noreply.github.com>
2024-10-15 16:35:52 +0300

    Time: 44 ms (38.31%), Space: 16.5 MB (77.14%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,18 +1,20 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        i=0
         res=[]
-        while i<len(s):
-            idx =s.rfind(s[i])
-            j =i+1
-            while j < idx:
-                jdx = s.rfind(s[j])
-                if jdx > idx:
-                    idx=jdx
-                j+=1
-            
-            res.append(idx+1)
-            s=s[idx+1:]
+        lastindex={}
+        for i,item in enumerate(s):
+            lastindex[item]=i
+        sum=0
+        j=0
+        end = 0
+        while j < len(s):
+            sum+=1
+            lst = lastindex[s[j]]
+            end = max(end,lst)
+            if j==end:
+                res.append(sum)
+                sum=0
+            j+=1
         return res
 
         
